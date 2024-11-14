f0efcf8b92b81d555b8d168cb638db2f472c7631
5hseok <ohhs1010@gmail.com>
2024-11-14 19:28:13 +0900

    :wrench: [REFACTOR] ChatService 삭제

--- src/main/java/com/prochord/server/config/RedisConfig.java
+++ src/main/java/com/prochord/server/config/RedisConfig.java
@@ -1,55 +1,55 @@
-package com.prochord.server.config;
-
-import com.prochord.server.service.ChatService;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.listener.ChannelTopic;
-import org.springframework.data.redis.listener.RedisMessageListenerContainer;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
-import org.springframework.stereotype.Component;
-
-@Configuration
-public class RedisConfig {
-
-    @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        // 호스트와 포트를 적절히 설정합니다. 기본값으로 localhost:6379을 사용합니다.
-        return new LettuceConnectionFactory("redis_cache", 6379);
-    }
-
-    @Bean
-    public RedisMessageListenerContainer redisContainer(RedisConnectionFactory connectionFactory,
-                                                        MessageListenerAdapter listenerAdapter) {
-        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
-        container.setConnectionFactory(connectionFactory);
-        container.addMessageListener(listenerAdapter, new ChannelTopic("chat"));
-        return container;
-    }
-
-    @Bean
-    public MessageListenerAdapter listenerAdapter(ChatService chatService) {
-        return new MessageListenerAdapter(chatService, "handleChatMessage");
-    }
-
-    @Bean
-    public ChannelTopic topic() {
-        return new ChannelTopic("chat");
-    }
-
-    @Bean
-    public RedisTemplate<String, Object> redisTemplate() {
-        // RedisTemplate 인스턴스를 만들고 직렬화 방법을 설정합니다.
-        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(redisConnectionFactory());
-
-        // key와 value의 직렬화 방식 설정
-        redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
-        return redisTemplate;
-    }
-}
+//package com.prochord.server.config;
+//
+//import com.prochord.server.service.ChatService;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+//import org.springframework.data.redis.core.RedisTemplate;
+//import org.springframework.data.redis.listener.ChannelTopic;
+//import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+//import org.springframework.data.redis.connection.RedisConnectionFactory;
+//import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
+//import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+//import org.springframework.data.redis.serializer.StringRedisSerializer;
+//import org.springframework.stereotype.Component;
+//
+//@Configuration
+//public class RedisConfig {
+//
+//    @Bean
+//    public RedisConnectionFactory redisConnectionFactory() {
+//        // 호스트와 포트를 적절히 설정합니다. 기본값으로 localhost:6379을 사용합니다.
+//        return new LettuceConnectionFactory("redis_cache", 6379);
+//    }
+//
+//    @Bean
+//    public RedisMessageListenerContainer redisContainer(RedisConnectionFactory connectionFactory,
+//                                                        MessageListenerAdapter listenerAdapter) {
+//        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+//        container.setConnectionFactory(connectionFactory);
+//        container.addMessageListener(listenerAdapter, new ChannelTopic("chat"));
+//        return container;
+//    }
+//
+//    @Bean
+//    public MessageListenerAdapter listenerAdapter(ChatService chatService) {
+//        return new MessageListenerAdapter(chatService, "handleChatMessage");
+//    }
+//
+//    @Bean
+//    public ChannelTopic topic() {
+//        return new ChannelTopic("chat");
+//    }
+//
+//    @Bean
+//    public RedisTemplate<String, Object> redisTemplate() {
+//        // RedisTemplate 인스턴스를 만들고 직렬화 방법을 설정합니다.
+//        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+//        redisTemplate.setConnectionFactory(redisConnectionFactory());
+//
+//        // key와 value의 직렬화 방식 설정
+//        redisTemplate.setKeySerializer(new StringRedisSerializer());
+//        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+//        return redisTemplate;
+//    }
+//}
