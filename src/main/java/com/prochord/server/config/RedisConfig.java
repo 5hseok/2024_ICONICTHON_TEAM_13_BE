2c2fbd8c2a416973ce0314c443ec741a5a675520
5hseok <ohhs1010@gmail.com>
2024-11-14 16:50:04 +0900

    :wrench: [REFACTOR] config 파일 수정

--- src/main/java/com/prochord/server/config/RedisConfig.java
+++ src/main/java/com/prochord/server/config/RedisConfig.java
@@ -3,10 +3,14 @@ package com.prochord.server.config;
 import com.prochord.server.service.ChatService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
@@ -28,5 +32,23 @@ public class RedisConfig {
     @Bean
     public ChannelTopic topic() {
         return new ChannelTopic("chat");
+    }
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        // 호스트와 포트를 적절히 설정합니다. 기본값으로 localhost:6379을 사용합니다.
+        return new LettuceConnectionFactory("localhost", 6379);
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        // RedisTemplate 인스턴스를 만들고 직렬화 방법을 설정합니다.
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+
+        // key와 value의 직렬화 방식 설정
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        return redisTemplate;
     }
 }
--- src/main/java/com/prochord/server/global/exception/auth/SecurityConfig.java
+++ src/main/java/com/prochord/server/global/exception/auth/SecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
@@ -38,6 +39,7 @@ public class SecurityConfig {
                 exception.accessDeniedHandler(customAccessDeniedHandler);
             })
             .authorizeHttpRequests(auth -> {
+                auth.requestMatchers(new AntPathRequestMatcher("/ws/chat/**")).permitAll();
                 auth.requestMatchers(AUTH_WHITE_LIST).permitAll();
                 auth.anyRequest().authenticated();
             })
